b7be562d17792db338e31989bbdbcf6415baacac
0Neokun0 <nishant.meher.9595@gmail.com>
2022-01-30 21:33:22 +0900

    Your commit message

---
+++ hungry.py
@@ -0,0 +1,9 @@
+hungry = input( "腹減っていますか?" ) # take user input
+if hungry == "はい":
+    print( "ラメンを食べたい!" )
+    print( "寿司を食べたい!" )
+    print( "バーガーを食べたい!" )
+else:
+    thirsty = input ( " 喉を乾いていますか?" )
+    if thirsty == "yes":
+        print( "ミスを飲んでください!" )
