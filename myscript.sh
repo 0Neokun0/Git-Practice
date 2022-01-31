9791f549ceb2ee4c80ffa98dbec5ed88cd536f91
0Neokun0 <nishant.meher.9595@gmail.com>
2022-01-31 13:26:02 +0900

    [if/fi]statements

--- myscript.sh
+++ myscript.sh
@@ -1,10 +1,18 @@
 
 
 # ECHO COMMAND エコーコマンド
-echo "Hello Everyone!"
+# echo "Hello Everyone!"
 
 # VARIABLES 変数
 # Uppercase by convention 慣例により大文字
 
-read -p "ENTER YOUR NAME :" NAME
-echo " Hello ${NAME}, はじめまして、よろしくお願いいたします。 "
+# read -p "ENTER YOUR NAME :" NAME
+# echo " Hello ${NAME}, はじめまして、よろしくお願いいたします。 "
+
+# SIMPLE IF STATEMENT
+NAME="Nishant"
+if [ "$NAME" == "Nishant" ]
+then 
+    echo " カタカナでお名前はニシャントです。"
+fi
+
