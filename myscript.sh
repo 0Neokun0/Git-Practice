c30f97d76181d97ce155e33fd9b5c89f84e31af3
0Neokun0 <nishant.meher.9595@gmail.com>
2022-01-31 13:10:23 +0900

    初めてのshellscript

--- myscript.sh
+++ myscript.sh
@@ -1,2 +1,10 @@
-#! \\wsl$\Ubuntu-20.04\home\nishant\git_workspace\bootcamp
-echo " Hello from shell script"
+
+
+# ECHO COMMAND エコーコマンド
+echo "Hello Everyone!"
+
+# VARIABLES 変数
+# Uppercase by convention 慣例により大文字
+
+read -p "ENTER YOUR NAME :" NAME
+echo " Hello ${NAME}, はじめまして、よろしくお願いいたします。 "
