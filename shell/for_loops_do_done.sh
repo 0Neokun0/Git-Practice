b1139c67a0116ed89f5363a25ee398c58f3fedb3
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-03 19:49:28 +0900

    2 loop styles

---
+++ shell/for_loops_do_done.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+#FOR LOOPS
+
+for numbers in '1 2 3 4 5'; do echo " $numbers "; done
+    
+#出力 1 2 3 4 5
+     
+
+#END
---
+++ shell/for_loops_range.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+#FOR LOOPS RANGE
+
+for numbers in {1..5}; do echo " $numbers "; done
+
+<<出力
+ 1
+ 2
+ 3
+ 4
+ 5
+出力
+
+#END
