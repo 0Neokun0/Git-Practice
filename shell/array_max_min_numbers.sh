5feca4cfb2afe4d9b243d7e3009ce728f3773c21
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-05 18:55:54 +0900

    ファイルarray_variables.shとarray_max_min_number.shレビューのためです。

---
+++ shell/array_max_min_numbers.sh
@@ -0,0 +1,48 @@
+    #! /usr/bin/bash  
+
+
+echo " 入力したい数字の数を入力してください : "
+
+    #ユーザーからの入力を取得。
+    read NUMBERS
+
+for(( i = 0; i < NUMBERS; i++ ))
+    do
+        echo " 数字 $((i+1)) を入力してください : "
+        read NUMBER_COUNT[$i]
+    done
+
+    #入力した数字を出力する。
+    echo " 入力された数字は : "
+
+for(( i = 0; i < NUMBERS; i++ ))
+    do
+        echo " ${NUMBER_COUNT[$i]} "
+    done
+
+    #メインループ
+    MIN=${NUMBER_COUNT[0]}
+    MAX=${NUMBER_COUNT[0]}
+
+for(( i = 0; i < NUMBERS; i++ ))
+do
+    
+    # 最小数ロジック
+if [ ${NUMBER_COUNT[$i]} -lt $MIN ]; 
+then
+    MIN=${NUMBER_COUNT[$i]}
+
+    
+    # 最大数ロジック
+elif [ ${NUMBER_COUNT[$i]} -gt $MAX ]; 
+then
+    MAX=${NUMBER_COUNT[$i]}
+fi
+
+done
+
+    # ECHO COMMAND 最小と最大の数値を出力する。
+        echo " 配列内の最小数は : $MIN "
+        echo " 配列の最大数は : $MAX "
+
+    # END  
---
+++ shell/array_variables.sh
@@ -0,0 +1,22 @@
+    #! /usr/bin/bash  
+
+fruits=( "リンゴ" "ミカン" "バナナ" "スイカ" )
+
+    # 配列の要素を1つずつ表示するリープ
+    # FOR LOOPS TO SHOW EACH FRUIT 
+for fruit in ${fruits[@]}
+do
+
+    # 配列にある要素を出力する。
+    # DISPLAY FRUITS array[0] to array [3]
+    echo " リストにある、果物は [ $fruit ] "
+done
+    # 配列内の要素の数
+    # TOTAL ELEMENTS IN THE ARRAY LIST
+    echo " リストにある、果物の数 " [ ${#fruits[@]} ]
+    
+    # 配列の出力
+    # DISPLAY THE ARRAY
+    echo " リストである、すべての果物は [ ${fruits[@]} ] "
+
+  # END
