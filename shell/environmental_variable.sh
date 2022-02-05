f4895a7c5c23763034f30f9092938eef2e3ce902
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-05 21:29:58 +0900

    ファイルenvironmental_variable.sh
       ファイルenvironmental_variable.sh
    ーのためです
    
    ファイルenvironmental_variable.sh
    ーのためです

---
+++ shell/environmental_variable.sh
@@ -0,0 +1,14 @@
+    #! /usr/bin/bash  
+
+    
+TODAYS_DATE=$(date  +"%B %-d, %Y")    # 環境変数
+echo "Today's date is $TODAYS_DATE"   # 例 : February 5, 2022
+
+export FULL_NAME="ニシャントーNishant" #export設定
+
+FULL_NAME=" ニシャントーNishant "      
+
+echo  " 今日のメッセージ : "
+
+echo -e " 今日も一日頑張りましょう  \n\t " " $FULL_NAME ! "
+
