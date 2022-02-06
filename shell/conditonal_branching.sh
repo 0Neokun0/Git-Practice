718d72da84129fbd13cc61d07a2e23c0177fde5c
0Neokun0 <nishant.meher.9595@gmail.com>
2022-02-07 08:47:16 +0900

    ファイルseq.sh と conditional_branching.shは終了しました。

---
+++ shell/conditonal_branching.sh
@@ -0,0 +1,56 @@
+    #! /usr/bin/bash  
+    #数を推測するゲーム  
+
+    #乱数 ( 1から10の間 )  
+random_number=$[($RANDOM % 10) +1]  
+
+while :
+
+do
+    # 入力する値 
+    read -p " 私は1から10の間の数を考えています。あなたの推測 : " number_1   
+
+    # 入力されたデータは数字かどうかのチェック  
+    number_2=`echo $number_1 | sed 's/[0-9]//g'`  
+
+    if [ ! -z $number_2 ]
+
+    then
+
+        # 例 : NG 文字組 ( a / z / ! / - / あ) 
+        echo " 値ではありません! "  
+
+        continue
+
+    fi
+
+    if [ $number_1 == $random_number ]
+
+    then
+
+        # 推測出来たらの出力  
+        echo -e " よくできた!! \n\t- お疲れ様です。" 
+
+        break
+
+    elif [ $number_1 -gt $random_number ]
+
+    then
+
+        # 推測の値は乱数ようり高い  
+        echo -e " 推測は高すぎます。新しい推測,お願いします ! " 
+
+        continue
+
+    else
+        # 推測の値は乱数ようり低い  
+        echo -e " 推測は低すぎます。新しい推測,お願いします !"  
+
+        continue
+
+    fi
+  
+    
+done    
+
+  #END  
---
+++ shell/seq.sh
@@ -0,0 +1,33 @@
+  #! /usr/bin/bash  
+  ## SEQ COMMAND  
+
+  #ECHO COMMANDS  
+
+echo -e " 開始値を入力してください : \c "
+
+  read -r starting_num  # 例 : 1  
+
+echo -e " 間隔値を入力してください : \c "
+
+  read -r interval_num  # 例 : 2  
+
+echo -e " 終了値を入力してください : \c "
+
+  read -r ending_num    # 例 : 10  
+
+  #FOR CONDITION  
+
+for number in `seq $starting_num $interval_num $ending_num`
+
+do
+    echo " $number "
+done
+
+<<出力
+ 1
+ 3
+ 5 
+ 7 
+ 9
+出力
+  #END  
