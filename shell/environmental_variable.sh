2082944f3e4977c4ff6cf40629541398f1027061
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-02-07 17:02:46 +0900

    Update environmental_variable.sh

--- shell/environmental_variable.sh
+++ shell/environmental_variable.sh
@@ -1,18 +1,22 @@
-#! /usr/bin/bash  
-
-# 環境変数  
-TODAYS_DATE=$(date  +"%B %-d, %Y")
-echo " Today's date is $TODAYS_DATE "
-# 例 : February 7, 2022  
+  #! /usr/bin/bash
+  #
+  # 環境変数
+  #
+  TODAYS_DATE=$(date  +"%B %-d, %Y")
+  echo " Today's date is $TODAYS_DATE "
+  # 例 : February 7, 2022
   
-# export設定  
-export FULL_NAME="ニシャントーNishant"
+  #
+  # export設定
+  #
+  export FULL_NAME="ニシャントーNishant"
+  # FULL_NAME設定
+  FULL_NAME=" ニシャントーNishant "
+  
+  #
+  # ECHO COMMAND
+  #
+  echo  " 今日のメッセージ : "
+  echo -e " 今日も一日頑張りましょう  \n\t " " $FULL_NAME ! "
 
-# FULL_NAME設定  
-FULL_NAME=" ニシャントーNishant "
-
-# ECHO COMMAND  
-echo  " 今日のメッセージ : "
-echo -e " 今日も一日頑張りましょう  \n\t " " $FULL_NAME ! "
-
-# END  
+  # END
