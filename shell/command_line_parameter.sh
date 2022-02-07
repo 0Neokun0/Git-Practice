e4aa0deb659c2e09f708ce4efa53f1eb7a9dfbb8
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-02-07 10:21:13 +0900

    command_line_parameter.sh_先頭行のスペース修正
    コメントの先頭行のスペースをなくしました。

--- shell/command_line_parameter.sh
+++ shell/command_line_parameter.sh
@@ -1,11 +1,10 @@
   #! /usr/bin/bash  
-
-  # ECHO COMMAND LINE
+  # ECHO COMMAND LINE  
 
 echo " 好きなプログラミング言語 : [ $1 ] "    
 echo " 好きなツール: [ $2 ]"                 
 echo " 好きなクラウドサーバー: [ $3 ] "       
 echo " 好きなi.t会社: [ $4 ] "              
 echo " 入力したデータの数 : [ $# ] "         
-
-    # END 
+  
+  # END  
