bec82ab88fd6f1e0f38455089c5e2d2c496d7b95
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-02-07 10:35:25 +0900

    environmental_variable.sh_先頭の位置修正
    コメントの先頭の位置を揃えました。

--- shell/environmental_variable.sh
+++ shell/environmental_variable.sh
@@ -1,15 +1,18 @@
-    #! /usr/bin/bash  
+  #! /usr/bin/bash  
 
-    
-TODAYS_DATE=$(date  +"%B %-d, %Y")     # 環境変数  
-echo " Today's date is $TODAYS_DATE "  # 例 : February 5, 2022  
+  # 環境変数  
+TODAYS_DATE=$(date  +"%B %-d, %Y")
+echo " Today's date is $TODAYS_DATE "
+  # 例 : February 7, 2022  
+  
+  # export設定 
+export FULL_NAME="ニシャントーNishant"
 
-export FULL_NAME="ニシャントーNishant"  #export設定  
-
+  # FULL_NAME設定  
 FULL_NAME=" ニシャントーNishant "
 
-    # ECHO COMMAND
+  # ECHO COMMAND  
 echo  " 今日のメッセージ : "
 echo -e " 今日も一日頑張りましょう  \n\t " " $FULL_NAME ! "
 
-    # END  
+  # END  
