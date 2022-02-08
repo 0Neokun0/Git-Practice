72ac65e3426b822321fa7dc3e711add91e243a31
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-02-08 21:52:48 +0900

    Indent Alignment

--- shell/execute-script-to-script/b.sh
+++ shell/execute-script-to-script/b.sh
@@ -1,9 +1,9 @@
-  #
-  #! /usr/bin/bash   
-  #
+#
+#! /usr/bin/bash   
+#
 
-  read -p " 値を入力してください : " number 
-  incremental_Number=`./a.sh $number` 
-  echo " 新しい値 : $incremental_Number " 
+read -p " 値を入力してください : " number 
+incremental_Number=`./a.sh $number` 
+echo " 新しい値 : $incremental_Number " 
 
-  # END
+# END
