05d812da78a03fd8f04f1da2395959f96bd4ebf5
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-02-08 21:20:40 +0900

    Indent Alignment

--- shell/conditonal_branching.sh
+++ shell/conditonal_branching.sh
@@ -8,37 +8,37 @@
 random_number=$[($RANDOM % 10) +1]  
 while :
 do
-  read -p " 私は1から10の間の数を考えています。あなたの推測 : " number_1
-  number_2=`echo $number_1 | sed 's/[0-9]//g'`
+read -p " 私は1から10の間の数を考えています。あなたの推測 : " number_1
+number_2=`echo $number_1 | sed 's/[0-9]//g'`
 
 #
 # 例 : NG 文字組 ( a / z / ! / - / あ)
 #
 if [ ! -z $number_2 ]　; then
-    echo " 値ではありません! "  
-    continue 
+  echo " 値ではありません! "  
+continue 
 fi
 
 #
 # 推測でき場合
 #
 if [ $number_1 == $random_number ] ; then
-    echo -e " よくできた!! \n\t- お疲れ様です。" 
-    break
+  echo -e " よくできた!! \n\t- お疲れ様です。" 
+break
 
 #  
 # 推測の値は乱数ようり高い
 #
 elif [ $number_1 -gt $random_number ] ; then
-    echo -e " 推測は高すぎます。新しい推測,お願いします ! " 
-    continue
+  echo -e " 推測は高すぎます。新しい推測,お願いします ! " 
+continue
   
 #
 # 推測の値は乱数ようり低い
 #
 else
-    echo -e " 推測は低すぎます。新しい推測,お願いします !"  
-    continue
+  echo -e " 推測は低すぎます。新しい推測,お願いします !"  
+ continue
 fi 
 done
 
