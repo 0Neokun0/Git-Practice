c3d1efa9350ccff9073170130aa5406455618c17
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-02-08 09:54:50 +0900

    Alignment Of Lines

--- shell/conditonal_branching.sh
+++ shell/conditonal_branching.sh
@@ -15,7 +15,7 @@
   # 例 : NG 文字組 ( a / z / ! / - / あ)
   #
   if [ ! -z $number_2 ]　; then
-  echo " 値ではありません! "  
+    echo " 値ではありません! "  
   continue 
   fi
   
@@ -23,21 +23,21 @@
   # 推測でき場合
   #
   if [ $number_1 == $random_number ] ; then
-  echo -e " よくできた!! \n\t- お疲れ様です。" 
+    echo -e " よくできた!! \n\t- お疲れ様です。" 
   break
   
   #  
   # 推測の値は乱数ようり高い
   #
   elif [ $number_1 -gt $random_number ] ; then
-  echo -e " 推測は高すぎます。新しい推測,お願いします ! " 
+    echo -e " 推測は高すぎます。新しい推測,お願いします ! " 
   continue
   
   #
   # 推測の値は乱数ようり低い
   #
   else
-  echo -e " 推測は低すぎます。新しい推測,お願いします !"  
+    echo -e " 推測は低すぎます。新しい推測,お願いします !"  
   continue
   fi 
   done
