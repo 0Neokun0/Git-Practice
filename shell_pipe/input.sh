f51fc31b8e86fee847a532f6fdf3a43c89c65979
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-02-08 21:24:31 +0900

    Indent Alignment

--- shell_pipe/input.sh
+++ shell_pipe/input.sh
@@ -1,13 +1,13 @@
-  #
-  #! /usr/bin/bash   
-  #
-  if [ -t 0 ] ; then
-    echo "
-        キーボードからのstdin
+#
+#! /usr/bin/bash   
+#
+if [ -t 0 ] ; then
+  echo "
+      キーボードからのstdin
     "
-  else
-    echo "
-        パイプまたはファイルからのstdin
+else
+  echo "
+      パイプまたはファイルからのstdin
     "
-  fi
-  # END  
+fi
+# END
