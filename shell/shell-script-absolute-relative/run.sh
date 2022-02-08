d184a8f544cc29645411a0adc4a8827e4841c003
Nishant Meher <90218986+0Neokun0@users.noreply.github.com>
2022-02-08 21:28:36 +0900

    Indent Alignment

--- shell/shell-script-absolute-relative/run.sh
+++ shell/shell-script-absolute-relative/run.sh
@@ -1,15 +1,15 @@
-  #
-  #! /usr/bin/bash   
-  #
+#
+#! /usr/bin/bash   
+#
 
 if [[ ${path_to_check:0:1} = '/' ]] ; then
-   echo "
-        絶対パスです。
+  echo "
+      絶対パスです。
    "
 else
-   echo "
-        相対パスです。
+  echo "
+      相対パスです。
    "
 fi
 
-  # END
+# END
